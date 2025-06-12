914bce04bf38e146e651e602c9ddd5b9694d410e
abarnar16 <abarnar.24@ichat.sp.edu.sg>
2025-06-12 23:12:16 +0800

    final

--- server.js
+++ server.js
@@ -223,9 +223,7 @@ app.post("/generate-do", (req, res) => {
 });
 // Optional SPA fallback route - serve index.html for any unknown GET route
 // Place this AFTER API routes, otherwise it will block POST routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-});
+
 
 // Start server
 const PORT = process.env.PORT || 3000;
